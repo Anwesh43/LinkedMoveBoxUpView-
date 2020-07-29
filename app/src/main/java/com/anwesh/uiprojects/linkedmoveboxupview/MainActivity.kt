1d40f02d3ff061a1aa950cdfaddbee954920b3de
Anwesh43 <anweshthecool0@gmail.com>
2020-07-30 03:29:31 +0530

    adding fullscreen support and activity in view

--- app/src/main/java/com/anwesh/uiprojects/linkedmoveboxupview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmoveboxupview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedmoveboxupview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.moveboxupview.MoveBoxUpView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MoveBoxUpView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
