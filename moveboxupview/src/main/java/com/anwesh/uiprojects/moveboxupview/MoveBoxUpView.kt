f1079885d04e4a2c0252260047f72289179b19a1
Anwesh43 <anweshthecool0@gmail.com>
2020-07-30 02:52:18 +0530

    adding constants and libraries

--- moveboxupview/src/main/java/com/anwesh/uiprojects/moveboxupview/MoveBoxUpView.kt
+++ moveboxupview/src/main/java/com/anwesh/uiprojects/moveboxupview/MoveBoxUpView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.moveboxupview
 /**
  * Created by anweshmishra on 30/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val sizeFactor : Float = 10f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val delay : Long = 20
