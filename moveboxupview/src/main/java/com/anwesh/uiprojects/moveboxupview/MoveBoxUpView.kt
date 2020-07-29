1eedb8b601305bfdea1700c64e03973160efc320
Anwesh43 <anweshthecool0@gmail.com>
2020-07-30 03:09:17 +0530

    adding MoveUpBoxView class

--- moveboxupview/src/main/java/com/anwesh/uiprojects/moveboxupview/MoveBoxUpView.kt
+++ moveboxupview/src/main/java/com/anwesh/uiprojects/moveboxupview/MoveBoxUpView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawMUBNode(i : Int, scale : Float, paint : Paint) {
     drawMoveUpBox(scale, w, h, paint)
     restore()
 }
+
+class MoveBoxUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
